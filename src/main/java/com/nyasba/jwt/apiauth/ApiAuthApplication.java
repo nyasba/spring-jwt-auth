9a32073debb04e3477193b1ba4e8840efd33dc1f
Naofumi Yasuba <n.yasuba@gxp.co.jp>
2018-01-03 00:06:57 +0900

    初回コミット（initializr）

---
+++ src/main/java/com/nyasba/jwt/apiauth/ApiAuthApplication.java
@@ -0,0 +1,12 @@
+package com.nyasba.jwt.apiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthApplication.class, args);
+	}
+}
